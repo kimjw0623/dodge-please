c5b93f405d745861105b70a45d5fc288cc49bd6d
Jungwoo Kim <kimjw0623@kaist.ac.kr>
2022-05-18 17:29:59 +0900

    modify training part

--- train.py
+++ train.py
@@ -3,33 +3,47 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import csv
 
-class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file, names=['file_name', 'label'])
-        self.img_dir = img_dir
-        self.transform = transform
-        self.target_transform = target_transform
-
+class EmbeddedDataset(torch.utils.data.Dataset):
+    def __init__(self, file_name):
+        # self.img_labels = pd.read_csv(annotations_file, names=['file_name', 'label'])
+        self.file_name = file_name
+        self.match_info_list, self.match_result_list = self._load_match_list()
+        
     def __len__(self):
-        return len(self.img_labels)
+        with open(self.file_name, mode='r', encoding='utf-8-sig') as inp:
+            reader = csv.reader(inp)
+            dataset_length = len(reader)
+        return dataset_length
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-        return image, label
+        single_match_info = self.match_info_list[idx]
+        single_match_result = self.match_result_list[idx]
+        return single_match_info, single_match_result
+    
+    def _load_match_list(self):
+        match_info_list = []
+        match_result_list = []
+        with open(self.file_name, mode='r', encoding='utf-8-sig') as inp:
+            reader = csv.reader(inp)
+            for rows in reader:
+                match_info = rows[0]
+                match_result = 0
+                if rows[0][0]:
+                    match_result = 1
+                
+                match_info_list.append(match_info)
+                match_result_list.append(match_result)
 
+        return match_info_list, match_result_list
+        
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(60, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 2)
+        self.fc3 = nn.Linear(84, 1)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -37,31 +51,53 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 
-net = Net()
+def binary_acc(y_pred, y_test):
+    y_pred_tag = torch.round(torch.sigmoid(y_pred))
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.parameters(), lr=0.001)
+    correct_results_sum = (y_pred_tag == y_test).sum().float()
+    acc = correct_results_sum/y_test.shape[0]
+    acc = torch.round(acc * 100)
+    
+    return acc
 
-for epoch in range(2):   # 데이터셋을 수차례 반복합니다.
+train_file_name = ''
+test_file_name = ''
+batch_size = 32
+total_epoch = 100
 
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # [inputs, labels]의 목록인 data로부터 입력을 받은 후;
-        inputs, labels = data
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
 
-        # 변화도(Gradient) 매개변수를 0으로 만들고
+train_dataset = EmbeddedDataset(file_name=train_file_name) 
+test_dataset = EmbeddedDataset(file_name=test_file_name) 
+
+training_generator = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=False)
+testing_generator = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False, drop_last=False)
+
+model = Net()
+
+criterion = nn.BCEWithLogitsLoss()#nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+model.train()
+for e in range(1, total_epoch):
+    epoch_loss = 0
+    epoch_acc = 0
+    for X_batch, y_batch in training_generator:
+        X_batch, y_batch = X_batch.to(device), y_batch.to(device)
         optimizer.zero_grad()
-
-        # 순전파 + 역전파 + 최적화를 한 후
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
+        
+        y_pred = model(X_batch)
+        
+        loss = criterion(y_pred, y_batch)#.unsqueeze(1))
+        acc = binary_acc(y_pred, y_batch)#.unsqueeze(1))
+        
         loss.backward()
         optimizer.step()
-
-        # 통계를 출력합니다.
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-            running_loss = 0.0
-
-print('Finished Training')
+        
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+        
+    avg_loss = epoch_loss/len(training_generator)
+    avg_acc = epoch_acc/len(training_generator)
+    print(f'epoch {e}: | Loss: {avg_loss:.5f} | Acc: {avg_acc:.3f}')
